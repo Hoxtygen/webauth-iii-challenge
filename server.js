5c54fa8e11aad31aaaa3e69e5c041fd349d8926d
Hoxtygen <hoxtygen@live.com>
2019-07-24 13:04:29 +0100

    Create server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const server = express();
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use(cors());
+
+server.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Welcome to jsonwebtoken class, enjoy your stay and do not forget to learn',
+  });
+});
+
+module.exports = server;
